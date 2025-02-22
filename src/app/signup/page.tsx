d27a70685eaa95e622986e2e46588c6eaa695f78
drh-14 <darrenh1134@gmail.com>
2025-02-22 16:26:42 -0500

    reformatted document

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,12 @@
 'use client'
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react';
-import {ThemeSupa} from '@supabase/auth-ui-shared';
-export default function SignUp(){
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+export default function SignUp() {
     const supabase = createClient('https://cvgvsplspmhoomurnsmy.supabase.co/', '<INSERT PROJECT ANON API KEY>')
-    const App = () => <Auth supabaseClient={supabase} providers={['google']} appearance={{theme: ThemeSupa}} view = "sign_up"/>
-    return(
-        <div className = 'mt-40 m-auto max-w-lg'>
+    const App = () => <Auth supabaseClient={supabase} providers={['google']} appearance={{ theme: ThemeSupa }} view="sign_up" />
+    return (
+        <div className='mt-40 m-auto max-w-lg'>
             <App></App>
         </div>
     )
