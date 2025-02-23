5738ec90476f0fdf977481ee88dad2bfe9587afe
drh-14 <darrenh1134@gmail.com>
2025-02-23 00:24:41 -0500

    merge

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { Pinecone } from "@pinecone-database/pinecone";
 import OpenAI from "openai";
 
@@ -8,42 +8,29 @@ const pinecone = new Pinecone({ apiKey: process.env.PINECONE_API_KEY as string }
 const indexName = "events-index";
 const index = pinecone.Index(indexName);
 
-export async function GET(request: NextRequest) {
-  const query = request?.nextUrl?.searchParams.get('text');
-
-  // Check if query parameter exists
-  if (!query) {
-    return new Response(JSON.stringify({ error: "Missing search query" }), {
-      status: 400,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
-
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const query = `Search for events with theme: ${body.theme}, category: ${body.category}, perk: ${body.perk}`;
   try {
     // Generate vector embedding using OpenAI
     const response = await openai.embeddings.create({
       model: "text-embedding-3-small",
       input: query,
     });
-
     const vector = response.data[0].embedding;
-
     // Query Pinecone with the generated vector
     const searchResults = await index.query({
       vector,
       topK: 10,
       includeMetadata: true,
     });
-
-    return new Response(JSON.stringify(searchResults), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' }
-    });
+    searchResults.matches.forEach((match) => console.log(match.metadata));
+    return NextResponse.json({data: searchResults.matches.forEach((match) => match.metadata)});
   } catch (error) {
     console.error(error);
 
     // Return 500 if there's an error during the process
-    return new Response(JSON.stringify({ error: `Internal server error: ${error}` }), {
+    return new Response(JSON.stringify({ error: `Internal server error: ${error}`}), {
       status: 500,
       headers: { 'Content-Type': 'application/json' }
     });
