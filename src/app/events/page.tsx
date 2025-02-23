8f3763adaa8bf44be602a317c17d751e76ec3620
Derrick Ma <derrickma2005@gmail.com>
2025-02-23 06:55:04 -0500

    Added key

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -30,7 +30,7 @@ export default function Events(){
         <div className = 'flex flex-col items-center mt-12 gap-8'>
             <h1 className = 'text-4xl'>Your Recommended Events</h1>
             <div className = 'flex flex-col items-center rounded-md gap-8 w-3/4'>
-            {eventList.map((e:Record<string,string>) => <EventTab event={e}/>)}
+            {eventList.map((e:Record<string,string>) => <EventTab key={e.title} event={e}/>)}
             </div>
             <Button variant = 'contained' onClick = {() => token ? router.push('/completionPage'): null}>{token ? "Transfer Events": "Log in or register"}</Button>
         </div>
