595ab06531ed6b66d8e10db952e41a5dede4e392
Derrick Ma <derrickma2005@gmail.com>
2025-02-22 21:12:25 -0500

    Updated colors

--- src/app/Components/sampleEvents.tsx
+++ src/app/Components/sampleEvents.tsx
@@ -1,12 +1,15 @@
 import {useEffect, useState} from 'react';
 import { createClient } from '@supabase/supabase-js';
+import {motion, useScroll, useTransform} from 'framer-motion';
+import { useRef } from 'react';
+import { section } from 'framer-motion/client';
 
 
 const supabaseUrl = 'https://cvgvsplspmhoomurnsmy.supabase.co/';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2Z3ZzcGxzcG1ob29tdXJuc215Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyNTQ1NzQsImV4cCI6MjA1NTgzMDU3NH0.CtuUnPiQqQNXP97LVwHDdf21cCfzaPdQ5kWQvLHY4Rc";
 const supabase = createClient(supabaseUrl, supabaseKey);
 async function getData() {
-  const { data, error } = await supabase.from('SampleEvents').select();
+  const { data, error } = await supabase.from('SampleEventsS').select();
   if (error) {
     console.error(error);
     return [];
@@ -25,13 +28,16 @@ export default function SampleEventWheel() {
     getData().then(event => setEvents(event));
   },[]);
   return (
-  <div className = 'flex flex-direction-row gap-8'>
-  {events.map((event, index) => (
-    <div className = 'h-40 w-72 border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg' key={index}>
+  <div  className = 'flex flex-direction-row gap-8'>
+  {events.map((event, index) => ( event ?
+    (<div className = 'h-40 w-72 border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg bg-red-200' key={index}>
       <p className = 'underline text-sm'>{event.title.split(" ").length < 10 ? event.title
       : event.title.split(" ").slice(0, 10).join(" ") + ". . ."}</p>
-      <p className = 'text-xs'>{event.description.split(" ").length < 30 ? event.description 
-      : event.description.split(" ").slice(0, 30).join(" ") + ". . ."}</p>
+      <p className = 'text-xs'>{event.description.split(" ").length < 25 ? event.description 
+      : event.description.split(" ").slice(0, 25).join(" ") + ". . ."}</p>
+    </div>)
+    : <div>
+      <p className = 'underline text-sm'>No events available</p>
     </div>
   ))}
   </div>);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,59 @@ import { Button } from '@mui/material';
 import Font, { Text } from 'react-font';
 import { useRouter } from 'next/navigation';
 import SampleEventWheel from './Components/sampleEvents';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
 
 export default function Home() {
+  const buttontheme = createTheme({
+    palette: {
+      primary: {
+        main: '#686868',
+      },
+      secondary: {
+        main: '#00ff00',
+      },
+    },
+  });
   const router = useRouter();
   return (
-    <div className="mb-7">
-      <div className='flex justify-end mt-6 mr-4 gap-4'>
-        <Button variant='outlined' size="medium" onClick ={() => router.push("/")}>Sign In</Button>
-        <Button variant='outlined'size="medium">Sign Up</Button>
+    <div className="mb-12">
+      <ThemeProvider theme={buttontheme}>
+      <div className='bg-green-400 flex justify-end p-4 gap-4 border-black border-b-4'>
+        <Button variant='contained' size="medium" onClick ={() => router.push("/")}>Sign In</Button>
+        <Button variant='contained'size="medium">Sign Up</Button>
       </div>
+      </ThemeProvider>
       <Font family="Funnel Sans">
         <div className='text-4xl flex flex-col gap-16 items-center mt-24'>
           <div className='flex flex-col gap-10 items-center'>
             <h1>Looking to get out of your dorm but keep missing club events?</h1>
             <h1>Not sure what events campus has to offer?</h1>
           </div>
-          <h1 className = 'text-5xl'>Introducing SBUWYD</h1>
+          <div className='flex flex-row gap-4 items-center justify-center'>
+          <p className = 'text-5xl'>Introducing</p>
+          <p className = 'text-5xl underline hover:text-7xl ease-out duration-300'>SBUWYD</p>
+          </div>
           <div className = 'flex gap-8'>
-            <div className = 'border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg'>
+            <div className = 'border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg bg-red-200'>
               <h1>Google Calendar Integration</h1>
               <div className = 'border-black border-2 w-full flex flex-col'></div>
-              <h2>Import events into Google Calendar to more easily track events of interest and make sure you don't miss any</h2>
+              <p className = 'text-[1.5rem]'>Import events into Google Calendar to more easily track events of interest and make sure you don't miss any</p>
             </div>
-            <div className = 'border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg'>
+            <div className = 'border-solid border-black border-2 p-4 rounded-md flex flex-col gap-4 max-w-lg bg-red-200'>
               <h1>Event Recommendation</h1>
               <div className = 'border-black border-2 w-full flex flex-col'></div>
-              <h2>Comprehensive recommender system to inform you of events based on your preferences and interests</h2>
+              <p className = 'text-[1.5rem]'>Comprehensive recommender system to inform you of events based on your preferences and interests</p>
             </div>
           </div>
           <div className = 'flex-col gap-8 items-center flex'>
             <h1>Current Events:</h1>
               <SampleEventWheel />
             </div>
-          <Button variant='contained' size="large">Get Started</Button>
+            <ThemeProvider theme={buttontheme}>
+          <Button className={"hover:drop-shadow-lg ease-in duration-300"} variant='contained' size="large">Find Tailored Events</Button>
+        </ThemeProvider>
         </div>
       </Font>
     </div>
