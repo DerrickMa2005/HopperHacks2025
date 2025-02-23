acb24627c24b2ec66fb2c2c9cdca0854c657b69e
Derrick Ma <derrickma2005@gmail.com>
2025-02-23 06:51:56 -0500

    Readded event tabs

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@mui/material';
 import { Navbar } from '../Components/navbar';
+import EventTab from '../Components/eventTab';
 export default function Events(){
     const router = useRouter();
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -28,14 +29,8 @@ export default function Events(){
         <Navbar/>
         <div className = 'flex flex-col items-center mt-12 gap-8'>
             <h1 className = 'text-4xl'>Your Recommended Events</h1>
-            <div className = 'flex flex-col items-center rounded-md gap-8 w-4/12'>
-            {eventList.map((e:Record<string,string>) => <div key = {e.title} className = 'flex flex-col border-2 border-black rounded-md p-4 border-solid items-center gap-4'>
-                <a className = 'text-sky-600' href={e.link} target="_blank">{e.title}</a>
-                <h1>{e.date}</h1>
-                <h1>{e.description}</h1>
-                <h1>{e.location}</h1>
-                {e.organizer === 'N/A' ? null: <h1>{e.organizer}</h1>}
-            </div>)}
+            <div className = 'flex flex-col items-center rounded-md gap-8 w-3/4'>
+            {eventList.map((e:Record<string,string>) => <EventTab event={e}/>)}
             </div>
             <Button variant = 'contained' onClick = {() => token ? router.push('/completionPage'): null}>{token ? "Transfer Events": "Log in or register"}</Button>
         </div>
